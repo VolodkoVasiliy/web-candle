e6c2914cac0b4b836623d3bcda7f6968ab2eb13a
Vasiliy Volodko <volodkovasya@gmail.com>
2025-08-08 21:29:24 +0200

    feat: add url

--- src/utils/auth.ts
+++ src/utils/auth.ts
@@ -2,9 +2,21 @@ import { betterAuth } from "better-auth";
 import Database from 'better-sqlite3'
 import { admin } from "better-auth/plugins"
 import { nextCookies } from "better-auth/next-js";
+import { db, db2 } from "./db";
+import { drizzleAdapter } from "better-auth/adapters/drizzle"
+import { account, session, user, verification } from '../../auth-schema'
+
 
 export const auth = betterAuth({
-    database: new Database("./sqlite.db"),
+    database: drizzleAdapter(db, {
+        provider: "sqlite", // or "mysql", "sqlite"
+        schema: {
+            verification,
+            user,
+            session,
+            account
+        }
+    }),
     emailAndPassword: {
         enabled: true,
     },
